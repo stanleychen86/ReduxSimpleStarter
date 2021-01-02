51b139465824ea19c1536eb82c9f21be726bfaad
stanleychen86 <47363544+stanleychen86@users.noreply.github.com>
2021-01-02 00:07:30 -0600

    new components added

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -12,6 +12,7 @@ class SearchBar extends Component {
     render() {
         return (
             <div>
+                value={this.state.term}
                 <input onChange={event => this.setState({term: event.target.value})} />
             </div>
             
--- src/index.js
+++ src/index.js
@@ -1,15 +1,37 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
-
+import YTSearch from 'youtube-api-search';
+import VideoList from './components/video_list';
 const API_KEY = 'AIzaSyAgqlNR3Ff1YR0AMCK_dBZ3-pE4BOmG0YA';
 
+
+
 //should produce some HTML
-const App = () => {
-    return <div>
+class App extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = {videos: []};
+
+        YTSearch({key: API_KEY, term: 'surfborads'}, (videos) =>{
+            //this.setState({videos:videos});
+            this.setState({videos});
+            
+        });
+    }
+
+
+    render() {
+    return (
+    <div>
         <SearchBar />
+        <VideoList videos={this.state.videos}/>
     </div>
+    );
+    }
 }
+
 
 //take this component's generated html and put it on the page (in the DOM)
 ReactDOM.render(<App />, document.querySelector('.container'));
